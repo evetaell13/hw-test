360e56c89ead163162f50eb8ad62a78ccf93975a
evetaell <evetaell1@gmail.com>
2022-07-20 16:45:40 +0400

    hw08_envdir_tool

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	envVars := []string{}
+
 	for name, val := range env {
-		envVars = append(envVars, fmt.Sprintf("%s=%s", name, val))
+		if val == "" {
+			os.Unsetenv(name)
+		} else {
+			os.Setenv(name, val)
+		}
 	}
 
-	c := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
-	c.Env = envVars
+	c := exec.Command(cmd[0], cmd[1:]...)
+
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
